751dd5f1b362cc615029422048e89385c76dbe05
Somesh Lad <35860510+SomeshLad@users.noreply.github.com>
2022-01-11 19:42:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="target"
+          target="target4"
           rel="noopener noreferrer"
         >
           Learn React
